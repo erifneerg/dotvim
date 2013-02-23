1a1bd025f10b83231d399d358636b53997f7db9f
Dorian Kernytsky <DorianK@gmail.com>
2013-02-23 00:52:48 -0500

    first attempt at making a install (shell) script

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +BundleInstall +qa
+echo "Make a folder for swap, undo, and backup in:"
+echo "  ~/.local/share/vim/undo on Linux"
+echo "  ~/Library/Vim/undo on OS X"
