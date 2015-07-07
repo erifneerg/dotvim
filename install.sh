7a4014809c3c8f93fa252376d44f15dbdcc8fdc5
Dorian Kernytsky <doriank@gmail.com>
2015-07-07 15:45:06 -0400

    Replaced the Vundle setup for NeoBundle

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ln -s ~/dotvim/vimrc ~/.vimrc 
-ln -s ~/dotvim/vim ~/.vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qa
+ln -s ~/dotvim ~/.vim
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > NeoInstall.sh
+sh ./NeoInstall.sh
 echo "Make a folder for swap, undo, and backup in:"
 echo "  ~/.local/share/vim/undo on Linux"
 echo "  ~/Library/Vim/undo on OS X"
