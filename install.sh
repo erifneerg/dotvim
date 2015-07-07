0a0b9641a70f313e309b105fe2570b2fc19c5578
Dorian Kernytsky <doriank@gmail.com>
2015-07-07 15:59:12 -0400

    Undo else for mac

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@ ln -s ~/dotvim/vimrc ~/.vimrc
 ln -s ~/dotvim ~/.vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > NeoInstall.sh
 sh ./NeoInstall.sh
-echo "Make a folder for swap, undo, and backup in:"
-echo "  ~/.local/share/vim/undo on Linux"
-echo "  ~/Library/Vim/undo on OS X"
+if [[ "$(uname)" == 'Darwin' ]]; then
+  mkdir -p ~/Library/Vim/undo
+  echo "  Undo folder made"
+else
+  echo "Make a folder for swap, undo, and backup in:"
+  echo "  ~/.local/share/vim/undo on Linux"
+fi
+echo $uname
