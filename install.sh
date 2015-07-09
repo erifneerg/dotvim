87673d718f43513d1f9a449acbd1bc1edf5eab6c
Dorian Kernytsky <DorianK@gmail.com>
2015-07-09 04:07:03 -0400

    Address an issue where neobundle doesn't install the bundles.

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@ ln -s ~/dotvim/vimrc ~/.vimrc
 ln -s ~/dotvim ~/.vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > NeoInstall.sh
 sh ./NeoInstall.sh
+vim +NeoBundleInstall +qall
+rm ./NeoInstall.sh
+echo "  Vim setup"
+
 if [[ "$(uname)" == 'Darwin' ]]; then
   mkdir -p ~/Library/Vim/undo
-  echo "  Undo folder made"
+  echo "  Undo folder was made"
 else
   echo "Make a folder for swap, undo, and backup in:"
   echo "  ~/.local/share/vim/undo on Linux"
 fi
-echo $uname
